c776f66fbf4738fd62b6e4031fbf0a06a71aa970
wutdequack <jonathanleejuler@gmail.com>
2021-10-02 16:40:17 +0800

    Creation of skeleton of program
    * Creation of ui, parser and common classes.
    * Allow for user to input.
    * Program will quit after bye command is issued.
    * May want to use regex for parsing in future versions

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,21 +1,80 @@
 package seedu.duke;
 
-import java.util.Scanner;
+import static seedu.duke.common.MagicValues.BYE_STRING;
+
+import seedu.duke.ui.TextUi;
+import seedu.duke.parser.Parser;
 
 public class Duke {
+    /**
+     * Global variable used to show if loop to get user input should continue running.
+     */
+    public static Boolean toContinue = true;
+
+    private TextUi ui;
+
     /**
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
-
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+        new Duke().run();
     }
+
+    /**
+     * Handles the running of the program.
+     */
+    private void run() {
+        start();
+        loopUntilByeInitiated();
+    }
+
+    /**
+     * Set up objects and prints Welcome Message.
+     */
+    private void start() {
+        ui = new TextUi();
+        ui.printWelcomeMessage();
+    }
+
+    /**
+     * Continues running until bye command is issued.
+     */
+    private void loopUntilByeInitiated() {
+        // Continue Running Loop until bye is called
+        while (toContinue) {
+            // Gets user input
+            String userInput = ui.getUserInput();
+            executeCommand(userInput);
+        }
+    }
+
+    /**
+     * Stops loop and print bye message.
+     */
+    public void handleByeSequence() {
+        toContinue = false;
+        ui.printGoodByeMessage();
+    }
+
+    /**
+     * Executes the command based on what is given by the user.
+     * @param userLine Line that is inputted by the user.
+     */
+    private void executeCommand(String userLine) {
+        String keyword = new Parser().getKeywordFromUserInput(userLine);
+
+        // Checks for the input for keywords
+        switch (keyword.toUpperCase()) {
+        case BYE_STRING:
+            // Stop loop and print Goodbye
+            handleByeSequence();
+            break;
+        default:
+            // Command is not recognized
+            ui.printUnknownCommandMessage();
+            break;
+        }
+
+    }
+
 }
