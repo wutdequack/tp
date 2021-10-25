b3ac33157ecd6002290b47dffe514950ca30b02e
rebecca <e0560032@u.nus.edu>
2021-10-25 09:32:36 +0800

    Fixed checkstyle

--- src/main/java/seedu/duke/exceptions/InvalidAddMedicineFormatException.java
+++ src/main/java/seedu/duke/exceptions/InvalidAddMedicineFormatException.java
@@ -1,6 +1,7 @@
 package seedu.duke.exceptions;
 
 import static seedu.duke.common.Messages.ADD_MED_FORMAT_MESSAGE;
+
 public class InvalidAddMedicineFormatException extends DukeException {
     public InvalidAddMedicineFormatException() {
         super(ADD_MED_FORMAT_MESSAGE);
