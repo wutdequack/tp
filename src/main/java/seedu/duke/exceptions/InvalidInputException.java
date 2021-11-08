7594ec831e983419b068514772f5cd5d08ac1543
an ruyi <78540645+ruyian@users.noreply.github.com>
2021-11-08 11:42:01 +0800

    Amend InvalidInputException variable name

--- src/main/java/seedu/duke/exceptions/InvalidInputException.java
+++ src/main/java/seedu/duke/exceptions/InvalidInputException.java
@@ -1,7 +1,7 @@
 package seedu.duke.exceptions;
 
 public abstract class InvalidInputException extends DukeException {
-    public InvalidInputException(String message) {
-        super("Correct input format: " + message);
+    public InvalidInputException(String correctInputMessage) {
+        super("Correct input format: " + correctInputMessage);
     }
 }
