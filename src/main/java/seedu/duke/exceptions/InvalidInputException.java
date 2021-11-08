e8dec045d15ab4c6dbc05748cfc26d49fd77f7ea
an ruyi <78540645+ruyian@users.noreply.github.com>
2021-11-08 11:43:00 +0800

    Add javadoc to the InvalidInputException class

--- src/main/java/seedu/duke/exceptions/InvalidInputException.java
+++ src/main/java/seedu/duke/exceptions/InvalidInputException.java
@@ -1,5 +1,9 @@
 package seedu.duke.exceptions;
 
+/**
+ * This is the abstract exception which are raised when there is invalid input of user command.
+ * The error message prompts the user the correct input of their command.
+ */
 public abstract class InvalidInputException extends DukeException {
     public InvalidInputException(String correctInputMessage) {
         super("Correct input format: " + correctInputMessage);
